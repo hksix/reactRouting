1910e7845f240b82eefc45787beb541899e72862
hksix <mh.haseeb@gmail.com>
2017-09-06 13:21:15 -0400

    Adds dynamic routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Jank from './Jank.js'
+import Thing from './Thing.js'
+import Bob from './Bob.js'
+
+const navs = [
+  <Jank />,
+  <Thing />,
+  <Bob />
+];
+
+const navNames=[
+  'Jank',
+  'Thing',
+  'Bob'
+]
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      currentNav:1
+    }
+  }
   render() {
+    const componentToRender = navs[this.state.currentNav];
+
+    const myNavs = navs.map((comp,idx) => (
+      <li><a href='#' onClick={(event) => {
+        event.preventDefault();
+        this._changeNav(idx);
+        }}>{navNames[idx]}</a></li>
+    ));
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <ul>
+            {myNavs}
+
+          </ul>
+
+        {componentToRender}
+        
       </div>
     );
+  }
+  _changeNav= (navValue) =>{
+    console.log(`hey:${navValue}`);
+    this.setState({
+      currentNav:navValue
+    })
   }
 }
 
